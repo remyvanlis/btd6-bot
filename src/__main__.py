47938cc629f1ccd657cb35756f0d0a4700dc85c3
Remy van Lis <remy.van.lis@windesheim.nl>
2022-05-23 13:52:27 +0200

    Actually fixed it this time

--- src/__main__.py
+++ src/__main__.py
@@ -73,8 +73,13 @@ def instructions():
             console.instasGained += 1
             console.wins += 1
             info['instructionHandler'].restart_after_freeplay()
-            console.instasPerHour = round(60.0 / ((round(console.currentTime - console.startTime) / console.instasGained) / 60), 2)
+            console.instasPerHour = round(60.0 / ((round((console.currentTime - console.startTime) / 1000) / console.instasGained) / 60), 2)
             info['instructionHandler'].start()
+
+        print(console.currentTime)
+        print(console.startTime)
+        print(console.instasGained)
+        print(console.currentTime - console.startTime)
 
         sleep(0.2)
 
